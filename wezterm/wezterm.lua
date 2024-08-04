1f44582c0bf94c81b0823662b2a9f1a82d533c1f
GridIland <koussougboss@gmail.com>
2024-08-04 19:06:43 +0000

    finalisaiton alacritty

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.enable_tab_bar = false
+config.enable_scroll_bar = true
+
+config.color_scheme = 'Galizur'
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+
+return config
